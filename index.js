65899ab48f92f3498eb983e0583de62b6b53ed09
ifham baig <ifhambaig001@gmail.com>
2024-05-16 22:44:47 +0530

    initial commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 require('dotenv').config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(cors());
 
